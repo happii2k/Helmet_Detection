ec7897c3d84d109b194f7a6958c60ca785d1173c
harsh <live.com#happywwe2k@outlook.com>
2026-02-05 12:49:33 +0000

    Pipeline Created

---
+++ src/pipeline/training_pipeline.py
@@ -0,0 +1,13 @@
+from src.components.ingestion import DataIngestion
+from src.components.validation import DataValidation
+from src.components.model_trainer import ModelTrainer
+data_ingestion = DataIngestion()
+data_validation = DataValidation()
+model_trainer = ModelTrainer()
+def run_pipeline():
+    data_ingestion.run()
+    data_validation.run_validation()
+    model_trainer.train()
+
+
+    
